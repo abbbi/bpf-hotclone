555007239b8e6e3565e1b171fcd9b4c374f14d48
Michael Ablassmeier <abi@grinser.de>
2021-09-26 18:11:08 +0200

    experimenting :)

--- hot-clone.py
+++ hot-clone.py
@@ -8,6 +8,7 @@ import signal, psutil
 from concurrent.futures import ProcessPoolExecutor as Pool
 
 src = "/dev/loop0"
+tgt = "DATA"
 
 device = os.stat(src).st_rdev
 minor = os.minor(device)
@@ -18,6 +19,7 @@ fh = open(src, "rb")
 
 done = False
 
+
 def kill_child_processes(parent_pid, sig=signal.SIGTERM):
     try:
         parent = psutil.Process(parent_pid)
@@ -27,6 +29,7 @@ def kill_child_processes(parent_pid, sig=signal.SIGTERM):
     for process in children:
         process.send_signal(sig)
 
+
 def callback(future):
     global done
     done = True
@@ -34,6 +37,7 @@ def callback(future):
         print("MAIN PROCESS EX")
     else:
         print("MAIN PROCESS EX")
+
 
 def trace_changes():
     with subprocess.Popen(
@@ -44,18 +48,19 @@ def trace_changes():
     ) as tracer:
         for line in iter(tracer.stdout.readline, ""):
             if b"REPLAY" in line:
+                time.sleep(0.1)
                 print(line.decode().strip())
                 rand = uuid.uuid4()
-                (_, sector, size, action) = line.decode().split(':')
-                fh.seek(int(sector)*512, 0)
+                (_, sector, size, action) = line.decode().split(":")
+                fh.seek(int(sector) * 512, 0)
                 with open("changeset/{}.{}".format(sector, rand), "wb") as targetfh:
                     targetfh.write(fh.read(int(size)))
+
 
 def main():
     print("begin waiting")
     pool = Pool(max_workers=2)
-    f = pool.submit(subprocess.run,
-        "dd if={} of=DATA".format(src), shell=True)
+    f = pool.submit(subprocess.run, "dd if={} of=DATA".format(src), shell=True)
     f.add_done_callback(callback)
 
     b = pool.submit(trace_changes)
@@ -68,21 +73,25 @@ def main():
     print("data copy finished")
 
     replayList = glob.glob("changeset/*")
+    time.sleep(10)
     if replayList:
         print("replaying changes")
         replayList = glob.glob("changeset/*")
         replayList.sort(key=os.path.getmtime)
-        tfh = open("DATA", "wb")
+        tfh = open(tgt, "wb")
+        tfh.seek(0)
         for repl in replayList:
             with open(repl, "rb") as rfh:
-                _ = repl.split('.')[0]
-                sector = _.split('/')[1]
-                tfh.seek(int(sector)*512, 0)
+                _ = repl.split(".")[0]
+                sector = _.split("/")[1]
+                tfh.seek(int(sector) * 512, 0)
                 tfh.write(rfh.read())
+                tfh.seek(0)
             print(sector)
         tfh.close()
 
     print("done")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
